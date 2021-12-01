407df3ae745b3e7043f9e8615d0d86c176b166d9
Luka Košćak <luka.koscak96@gmail.com>
2021-12-01 18:20:17 +0100

    New months can now be added

--- habbit-tracker/src/App.js
+++ habbit-tracker/src/App.js
@@ -12,23 +12,26 @@ const App = () => {
 	const habbitCtx = useHabbitContext();
 	const [dateFilter, setDateFilter] = useState(moment());
 
-	const selectedMonth = habbitCtx.months.find(
-		(month) => month.id === `${dateFilter.month() + 1}_${dateFilter.year()}`
-	);
-	if (!selectedMonth) {
-		habbitCtx.addMonth({
-			id: `${dateFilter.month() + 1}_${dateFilter.year()}`,
-			days: [...Array(dateFilter.daysInMonth()).keys()].map((_key) => {
-				return [];
-			}),
-		});
-	}
-
 	const monthId = `${dateFilter.month() + 1}_${dateFilter.year()}`;
 	const filterTitle = dateFilter.format("MMMM yyyy.");
 	const numOfDaysInMonth = dateFilter.daysInMonth();
 
 	const changeMonthHandler = (value) => {
+		const newDate =
+			value > 0
+				? moment(dateFilter).add(value, "months")
+				: moment(dateFilter).subtract(Math.abs(value), "months");
+		const selectedMonth = habbitCtx.months.find(
+			(month) => month.id === `${newDate.month() + 1}_${newDate.year()}`
+		);
+		if (!selectedMonth) {
+			habbitCtx.addMonth({
+				id: `${newDate.month() + 1}_${newDate.year()}`,
+				days: [...Array(newDate.daysInMonth()).keys()].map((_key) => {
+					return [];
+				}),
+			});
+		}
 		setDateFilter((currentDateFilter) => {
 			const newDate =
 				value > 0
